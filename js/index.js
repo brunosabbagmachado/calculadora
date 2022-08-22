0ce574c95f4caef4e5debdab0fad7e695e55dfdd
Bruno S. Machado <bruno.sabbagmachado@outlook.com>
2022-08-22 11:23:27 -0300

    criando logica para exibir os numeros clicados

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-var teste = 1;
+var display = document.querySelector('.calculadora__display');
+
+const numeros = document.querySelectorAll('[data-numero]');
+
+const operadores = document.querySelectorAll('[data-operador]');
+
+for (var i = 0; i < numeros.length; i++) {
+    numeros[i].addEventListener('click', function () {
+        display.innerHTML += parseInt(this.innerHTML);
+    })
+}
+
+for (var i = 0; i < operadores.length; i++) {
+    operadores[i].addEventListener('click', function () {
+        display.innerHTML += this.innerHTML;
+    })
+}
+
+console.log(display);
